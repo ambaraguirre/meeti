ca99dfbb3efeb7096daf5b570a22ab85dd18efd1
ambaraguirre <ambaraguirre67@gmail.com>
2024-08-01 16:53:29 -0600

    Terminando la funcionalidad de los mapas con leaflet y geosearch

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,15 @@
-import {OpenStreetMapProvider} from 'leaflet-geosearch';
+import { OpenStreetMapProvider } from "leaflet-geosearch";
 
-
-const lat =  19.24997;
-const lng =  -103.72714;
+const lat = 19.24997;
+const lng = -103.72714;
+var map = L.map('mapa').setView([lat, lng], 15);
+let markers = new L.FeatureGroup().addTo(map);
 let marker;
 
-var map = L.map('mapa').setView([lat, lng], 15);
 
-document.addEventListener('DOMContentLoaded', () =>{
+
+
+document.addEventListener('DOMContentLoaded', () => {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
@@ -17,24 +19,58 @@ document.addEventListener('DOMContentLoaded', () =>{
     buscador.addEventListener('input', buscarDireccion);
 })
 
-function buscarDireccion(e){
-    if(e.target.value.length > 8){
-        //utilizar provider
+function buscarDireccion(e) {
+    if (e.target.value.length > 8) {
+
+        //si existe un pin anterior limpiarlo
+        markers.clearLayers();
+
+        // Utilizar el provider y GeoCoder
+        const geocodeService = L.esri.Geocoding.geocodeService();
         const provider = new OpenStreetMapProvider();
-        provider.search({query: e.target.value}).then((resultado)=>{
-            //mostrar el mapa
-            map.setView(resultado[0].bounds[0], 15);
-            
-            //agregar el pin
-            marker = new L.marker(resultado[0].bounds[0], {
-                draggable: true,
-                autoPan: true
+        provider.search({ query: e.target.value }).then((resultado) => {
+            geocodeService.reverse().latlng(resultado[0].bounds[0], 15).run(function (error, result) {
+                llenarInputs(result);
+                //mostrar el mapa
+                map.setView(resultado[0].bounds[0], 15);
+
+                //agregar el pin
+                marker = new L.marker(resultado[0].bounds[0], {
+                    draggable: true,
+                    autoPan: true
+                })
+                    .addTo(map)
+                    .bindPopup(resultado[0].label)
+                    .openPopup()
+
+                //detectar movimiento del marker
+                marker.on('moveend', function (e) {
+                    marker = e.target;
+                    const posicion = marker.getLatLng();
+                    map.panTo(new L.latLng(posicion.lat, posicion.lng));
+
+                    //reverse geocoding cuando el usuario reubica el pin
+
+                    geocodeService.reverse().latlng(posicion, 15).run(function (error, result) {
+                        llenarInputs(result);
+                       //asigna los valores al marker popup
+                       marker.bindPopup(result.address.LongLabel);
+                    });
+                })
+                //asignar al contenedor
+                markers.addLayer(marker);
             })
-            .addTo(map)
-            .bindPopup(resultado[0].label)
-            .openPopup()
         })
     }
+}
+
+function llenarInputs(resultado){
+    document.querySelector('#direccion').value = resultado.address.Address || '';
+    document.querySelector('#ciudad').value = resultado.address.City || '';
+    document.querySelector('#estado').value = resultado.address.Region || '';
+    document.querySelector('#pais').value = resultado.address.CountryCode || '';
+    document.querySelector('#lat').value = resultado.latlng.lat || '';
+    document.querySelector('#lng').value = resultado.latlng.lng || '';
 }
 
 
